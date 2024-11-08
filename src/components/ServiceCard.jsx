baa3d10cefdf7e021a5e0778d8d5a320ba82b7ef
Khantminaung313 <khantminaung313@gmail.com>
2024-11-08 11:37:03 +0630

    fixed overflow problem in popup box

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,7 +4,7 @@ const ServiceCard = ({ service, onClick }) => {
 			<div className="mb-6 flex justify-center">
 				<img className="size-10 sm:size-16 lg:size-20" src={service.icon_path} alt="icon" />
 			</div>
-			<h3 className="card-title"><button onClick={onClick}>{service.title}</button></h3>
+			<h3 className="card-title cursor-pointer" onClick={onClick}>{service.title}</h3>
 			<p className="card-intro">{service.intro}</p>
 			<button className="card-button" onClick={onClick}>
 				<span>See More</span>
--- src/components/ServicePopUp.jsx
+++ src/components/ServicePopUp.jsx
@@ -18,23 +18,24 @@ const ServicePopUp = ({ selectedService, onClick }) => {
 		});
 	};
 	return (
-		<div className="fixed top-0 left-0 w-full h-full bg-slate-700/80 flex items-center justify-center z-50">
-			<div className="service-detail w-[90%] sm:max-w-[500px] lg:max-w-[700px] max-h-[80vh] mx-auto bg-white shadow p-8 lg:p-6 rounded relative">
+		<div className="fixed top-0 left-0 w-full h-full bg-slate-700/50 flex items-center justify-center z-40">
+			<div className="service-detail relative w-[90%] sm:max-w-[500px] lg:max-w-[700px] z-40 mx-auto bg-white shadow rounded">
 				<button
-					className="absolute -right-4 -top-4 inline-block px-4 py-2 border bg-slate-600 text-white rounded-full z-10"
+					className="absolute -right-4 -top-4 inline-block px-4 py-2 border bg-slate-600 text-white rounded-full z-50"
 					onClick={handleClose}
 				>
 					x
 				</button>
-				<h3 className="text-soft_blue text-2xl font-semibold mb-6">
-					{selectedService.title}
-				</h3>
 
-				<article className="w-full h-full overflow-y-auto">
+				<div className="max-h-[80vh] overflow-y-auto p-8 lg:p-6">
+					<h3 className="text-soft_blue text-center text-2xl font-semibold mb-6">
+						{selectedService.title}
+					</h3>
+
 					<p className="text-base leading-8 tracking-wide mb-4 text-justify text-slate-800 indent-8 font-poppins">
 						{selectedService.content}
 					</p>
-				</article>
+				</div>
 			</div>
 		</div>
 	);
